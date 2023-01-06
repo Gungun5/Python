56c6159895e3c79aeac77d32e5f99e1ca2c32fe1
Gungun5 <121714752+Gungun5@users.noreply.github.com>
2023-01-06 13:31:40 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,19 @@
+#variables are used to storing data.
+#variables are container for storing data.
+#for example :- In kitchen, we have containers for storing daal,rice and other things
+#similarly variables are also used for storing data
+#rules to make variable:
+#1. A variable name must start with a letter or the underscore character
+#2. A variable name cannot start with a number
+#3. A variable name can only contain aplha-numeric character and underscores
+#4. Variable names are case-sensitive. for ex.:- name and Name they are two different variable
+# name and Name
+#let's create a variable
+a = 5 # a stores integer 5
+b = 8 # b stores integer 8
+c = a+b 
+print(c)
+name = 'gungun' # name stores gungun as string and note that string always written under quotes. two types of quotes:- single'' or double""
+classs = "CSE" # double quote works similar as single quote
+# note that (class) is keyword so we can't create class name variable, you can write Class ,classs
+d= 2.5 # d stores float number
