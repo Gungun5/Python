f887ffb377a3d9db3f09db689db2134fbf92a6a0
Gungun5 <121714752+Gungun5@users.noreply.github.com>
2023-01-21 12:56:12 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,13 @@
+# comments are used for documentation 
+# comments can be used to make the code more readable
+# there are two types of comments in python
+# single line comment and multiline comment
+# Single line Comments starts with a #, and Python will ignore them:
+# this is single line comments
+# double line comments start with three single qoutes ''' and end with three single quotes ''' 
+'''
+hello
+my name is gungun
+i'm student
+how are you?
+'''
