e133528b107cda6a107a05fdcd798e0008083971
Gungun5 <121714752+Gungun5@users.noreply.github.com>
2023-01-21 15:48:24 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,19 @@
+'''
+Python has two primitive loop commands:
+
+while loops
+for loops
+
+The while Loop:-
+With the while loop we can execute a set of statements as long as a condition is true.
+'''
+#Example 1.:-
+i=0
+while i<11:
+    print(i)
+    i+=1
+#example 2.:-
+i=1
+while i<11:
+    print('2 X',i,'=',2*i)
+    i+=1
