5556676101b82b319c2c49bd024b286bca3d5ccd
Gungun5 <121714752+Gungun5@users.noreply.github.com>
2023-01-21 12:43:04 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,29 @@
+#data type means type of data 
+#Variables can store data of different types, and different types can do different things.
+#there are many types of data types in python :-
+#integer,string,boolean,float,complex,list,tuple,dict,set,frozenset,range,bytes,bytearray,memoryview,nonetype
+#note:- In python, there is no need to declare data type with variable
+#let's take an example of integer data type
+#integer data type also called int
+#int stores whole number 
+a = 5 # a stores 5 int 
+print(a)
+# To know which type of data stores in variable?
+# So we can use type() 
+print(type(a))
+#let's take an example of String 
+# String data type also called str
+#string is collection of characters
+b ='Hello'
+print(b)
+print(type(b))
+#let's take an example of float
+#float stores decimal number or point number
+c = 20.5
+print(c)
+print(type(c))
+#let's take an example of complex
+#complex stores alpha-numeric data
+d = 'hello1'
+print(d)
+print(type(d))
