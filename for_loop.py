f674ee0cd262774b3798b820a869b43702ba38bd
Gungun5 <121714752+Gungun5@users.noreply.github.com>
2023-01-21 16:33:33 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+'''
+A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+
+This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
+
+With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+'''
+#example 1.:-
+for i in range(0,10,1):
+    print(i)
+#example 2.:-
+for j in range(1,11,1):
+    print('2 X',j,'=',2*j)
+
