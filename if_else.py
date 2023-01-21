4f954c0d57ec7aa6c66c261d1ca803f873572e59
Gungun5 <121714752+Gungun5@users.noreply.github.com>
2023-01-21 15:29:15 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,13 @@
+#if statement is the most simple decision-making statement. 
+# It is used to decide whether a certain statement or block of statements will be executed or not i.e if a certain condition is true then a block of statement is executed otherwise not.
+# syntax :- 
+''' if(condition):
+            // block of code
+    else: # else if optional
+           // block of code
+'''
+a = 5
+if a==10:
+    print(a, ' is 10')
+else:
+    print(a, 'is not 10')
